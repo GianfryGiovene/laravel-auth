2677ff6081d68a5ec25005cc0886c6005770b9a2
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-05-30 15:52:19 +0200

    create post model

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+// rotte per autenticazione gestite automaticamente
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('admin/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function(){
+        Route::get('/','HomeController@index')
+        ->name('home');
+    });
+
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where('any', '.*');
