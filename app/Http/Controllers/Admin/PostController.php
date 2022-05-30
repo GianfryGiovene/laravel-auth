85a95bb5f3620236507937a328c01b9133c5d6dc
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-05-30 16:15:50 +0200

    creat CRUD added

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $post = Post::all();
+        return view('admin.posts.index',compact('post'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'title'=> 'required|max:250',
+            'content'=>'required'
+        ]);
+        $postData = $request->all();
+        $newPost = new Post();
+        $newPost->fill($postData);
+        $slug = Str::slug($newPost->title);
+        $alternativeSlug = $slug;
+        $postFound = Post::where('slug',$slug)->first();
+        $count = 1;
+        while($postFound){
+            $altSlug = $slug.'_'.$counter;
+            $count++;
+            $postFound = Post::where('slug',$altSlug)->first();
+        }
+        $newPost->slug = $alternativeSlug;
+        $newPost->save();
+        return redirect()->route('admin.posts.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
